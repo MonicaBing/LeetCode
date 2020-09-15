e0bf9595f5ea0aaa8ebf04d088d474bddc3cb4dd
MonicaBing <47180942+MonicaBing@users.noreply.github.com>
2020-09-15 11:17:46 +0100

    Add files via upload

---
+++ problems/1389.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+
+
+
+    
+def createTargetArray(nums, index):
+    output = []
+
+    for i in range (0,len(index)):
+        print(i)
+        
+        output.insert(index[i], nums[i])
+    
+    return output
+
